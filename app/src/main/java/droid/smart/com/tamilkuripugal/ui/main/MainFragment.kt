c0caff3886af2bc81459bb5732c327bc8c398e00
SmartDroidies <smartdroidies@gmail.com>
2019-01-03 07:28:50 -0500

    AndroidX Migration

--- app/src/main/java/droid/smart/com/tamilkuripugal/ui/main/MainFragment.kt
+++ app/src/main/java/droid/smart/com/tamilkuripugal/ui/main/MainFragment.kt
@@ -1,14 +1,14 @@
 package droid.smart.com.tamilkuripugal.ui.main
 
-import android.arch.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import droid.smart.com.tamilkuripugal.R
 
-class MainFragment : Fragment() {
+class MainFragment : androidx.fragment.app.Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
--- app/src/main/java/droid/smart/com/tamilkuripugal/ui/main/MainViewModel.kt
+++ app/src/main/java/droid/smart/com/tamilkuripugal/ui/main/MainViewModel.kt
@@ -1,6 +1,6 @@
 package droid.smart.com.tamilkuripugal.ui.main
 
-import android.arch.lifecycle.ViewModel
+import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
