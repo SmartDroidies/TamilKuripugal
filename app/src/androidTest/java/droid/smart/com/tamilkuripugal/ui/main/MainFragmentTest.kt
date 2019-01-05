702bf323e6a057e52c7c3d7fe4fa34c324c2d7a9
SmartDroidies <smartdroidies@gmail.com>
2019-01-05 15:56:51 -0500

    MainFragment Testing Skeleton

--- app/src/androidTest/java/droid/smart/com/tamilkuripugal/ui/main/MainFragmentTest.kt
+++ app/src/androidTest/java/droid/smart/com/tamilkuripugal/ui/main/MainFragmentTest.kt
@@ -2,8 +2,12 @@ package droid.smart.com.tamilkuripugal.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavController
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import droid.smart.com.tamilkuripugal.R
 import droid.smart.com.tamilkuripugal.binding.FragmentBindingAdapters
 import droid.smart.com.tamilkuripugal.testing.SingleFragmentActivity
 import droid.smart.com.tamilkuripugal.util.CountingAppExecutorsRule
@@ -12,7 +16,9 @@ import droid.smart.com.tamilkuripugal.util.TaskExecutorWithIdlingResourceRule
 import droid.smart.com.tamilkuripugal.util.mock
 import droid.smart.com.tamilkuripugal.vo.Category
 import droid.smart.com.tamilkuripugal.vo.Resource
+import org.hamcrest.CoreMatchers
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -34,14 +40,24 @@ class MainFragmentTest {
     private lateinit var viewModel: MainViewModel
     private lateinit var mockBindingAdapter: FragmentBindingAdapters
     //private val userData = MutableLiveData<Resource<User>>()
-    private val repoListData = MutableLiveData<Resource<List<Category>>>()
+    private val categoryListData = MutableLiveData<Resource<List<Category>>>()
     private val testFragment = TestMainFragment().apply {
         //arguments = UserFragmentArgs.Builder("foo").build().toBundle()
     }
 
 
+    @Test
+    fun loading() {
+        categoryListData.postValue(Resource.loading(null))
+        Espresso.onView(ViewMatchers.withId(R.id.progress_bar))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.retry))
+            .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
+    }
+
     class TestMainFragment : MainFragment() {
         val navController = mock<NavController>()
+        override fun navController() = navController
     }
 
 }
