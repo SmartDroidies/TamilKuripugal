2ad8e5609d1beb450ec6ee2d5f2edb457c49f3b6
SmartDroidies <smartdroidies@gmail.com>
2019-01-26 15:04:06 -0500

    Settings Basic Screen

--- app/src/main/java/droid/smart/com/tamilkuripugal/ui/main/MainViewModel.kt
+++ app/src/main/java/droid/smart/com/tamilkuripugal/ui/main/MainViewModel.kt
@@ -13,18 +13,20 @@ import javax.inject.Inject
 @OpenForTesting
 class MainViewModel @Inject constructor(categoryRepository: CategoryRepository) : ViewModel() {
 
-    //FIXME - Collect this from database
-    val CATEGORY_DATA = listOf(
-        Category("CTGRY01", "HEALTH", "Health Tips", 1, 5, null, R.drawable.arokiyam, "health"),
-        Category("CTGRY02", "BEAUTY", "Beauty Tips", 2, 6, null, R.drawable.azagu, "beauty"),
-        Category("CTGRY03", "TREATMENT", "Home Remedies", 3, 10, null, R.drawable.naattu, "home-remedies"),
-        Category("CTGRY04", "COOKING", "Cooking Tips", 4, 3, null, R.drawable.samayal, "cooking"),
-        Category("CTGRY05", "VETTU", "House Tips", 5, 1096, null, R.drawable.vettu, "house-keeping"),
-        Category("CTGRY06", "MARUTHUVAM", "Medical Tips", 6, 1095, null, R.drawable.maruthuvam, "medicine"),
-        Category("CTGRY07", "AANMEEGAM", "Divine Tips", 7, 7236, null, R.drawable.aanmeega, "aanmeegam"),
-        Category("CTGRY08", "GENERAL", "General Tips", 8, 2920, null, R.drawable.general, "general-tip"),
-        Category("CTGRY09", "AGRICULTURE", "Agricultrue Tips", 9, 9479, null, R.drawable.agriculture, "agriculture")
-    )
+    companion object {
+        //FIXME - Collect this from database
+        val CATEGORY_DATA = listOf(
+            Category("CTGRY01", "HEALTH", "Health Tips", 1, 5, null, R.drawable.arokiyam, "health"),
+            Category("CTGRY02", "BEAUTY", "Beauty Tips", 2, 6, null, R.drawable.azagu, "beauty"),
+            Category("CTGRY03", "TREATMENT", "Home Remedies", 3, 10, null, R.drawable.naattu, "home-remedies"),
+            Category("CTGRY04", "COOKING", "Cooking Tips", 4, 3, null, R.drawable.samayal, "cooking"),
+            Category("CTGRY05", "VETTU", "House Tips", 5, 1096, null, R.drawable.vettu, "house-keeping"),
+            Category("CTGRY06", "MARUTHUVAM", "Medical Tips", 6, 1095, null, R.drawable.maruthuvam, "medicine"),
+            Category("CTGRY07", "AANMEEGAM", "Divine Tips", 7, 7236, null, R.drawable.aanmeega, "aanmeegam"),
+            Category("CTGRY08", "GENERAL", "General Tips", 8, 2920, null, R.drawable.general, "general-tip"),
+            Category("CTGRY09", "AGRICULTURE", "Agricultrue Tips", 9, 9479, null, R.drawable.agriculture, "agriculture")
+        )
+    }
 
 
     //        {"code": "HEALTH", "id": "5", "label": "pirivu.health", "order": 3, "color" : "blue", "imageUrl": "images/arokiyam.png", "data" : "files/health.json", "topic" : "health"},
--- app/src/main/java/droid/smart/com/tamilkuripugal/ui/settings/SettingsFragment.kt
+++ app/src/main/java/droid/smart/com/tamilkuripugal/ui/settings/SettingsFragment.kt
@@ -1,13 +1,45 @@
 package droid.smart.com.tamilkuripugal.ui.settings
 
 
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
+import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
-import com.smart.droid.tamil.tips.R
+import androidx.preference.SwitchPreferenceCompat
+import droid.smart.com.tamilkuripugal.ui.main.MainViewModel
+import droid.smart.com.tamilkuripugal.ui.util.Helper
+import timber.log.Timber
+
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
+    val categories = MainViewModel.CATEGORY_DATA
+
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        val context = preferenceManager.context
+        val screen = preferenceManager.createPreferenceScreen(context)
+
+        val notificationCategory = PreferenceCategory(context)
+        notificationCategory.key = "notifications_category"
+        notificationCategory.title = "Notifications"
+        screen.addPreference(notificationCategory)
+
+        for (category in categories) {
+            Timber.d("Category : %s", category)
+            val notificationPreference = SwitchPreferenceCompat(context)
+            notificationPreference.key = category.code
+            notificationPreference.title = Helper.localeText(this.context!!, category.code)
+            notificationCategory.addPreference(notificationPreference)
+        }
+
+        var prefListener: OnSharedPreferenceChangeListener =
+            OnSharedPreferenceChangeListener { prefs, key ->
+                Timber.d("Pref Changed : %s - %s", key, prefs.getBoolean(key, false))
+            }
+        screen.sharedPreferences.registerOnSharedPreferenceChangeListener(prefListener)
+
+        preferenceScreen = screen
     }
+
 }
