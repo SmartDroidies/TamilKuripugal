0708c352803c9e6adb367fbf06492b4b9df9292d
SmartDroidies <smartdroidies@gmail.com>
2019-01-27 05:17:47 -0500

    Fabric crash reporting

--- app/src/main/java/droid/smart/com/tamilkuripugal/KuripugalApp.kt
+++ app/src/main/java/droid/smart/com/tamilkuripugal/KuripugalApp.kt
@@ -5,13 +5,19 @@ import android.app.Application
 import android.content.Context
 import android.content.res.Configuration
 import android.text.TextUtils
+import android.util.Log
+import androidx.annotation.Nullable
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.smart.droid.tamil.tips.BuildConfig
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import droid.smart.com.tamilkuripugal.di.AppInjector
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 import java.util.*
 import javax.inject.Inject
+
 
 class KuripugalApp : Application(), HasActivityInjector {
 
@@ -22,11 +28,16 @@ class KuripugalApp : Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
+
+        val core = CrashlyticsCore.Builder()
+            .disabled(BuildConfig.DEBUG)
+            .build()
+        Fabric.with(this, Crashlytics.Builder().core(core).build())
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
-        } else {
-            //FIXME Create a Crashanalytics tree for Timber
         }
+        Timber.plant(CrashlyticsTree())
         AppInjector.init(this)
         setDefaultLocale(this)
     }
@@ -42,4 +53,30 @@ class KuripugalApp : Application(), HasActivityInjector {
 
     override fun activityInjector() = dispatchingAndroidInjector
 
+    inner class CrashlyticsTree : Timber.Tree() {
+
+        private val CRASHLYTICS_KEY_PRIORITY = "priority"
+        private val CRASHLYTICS_KEY_TAG = "tag"
+        private val CRASHLYTICS_KEY_MESSAGE = "message"
+
+        override fun log(priority: Int, @Nullable tag: String?, @Nullable message: String, @Nullable t: Throwable?) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+                return
+            }
+
+            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority)
+            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag)
+            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message)
+
+            if (t == null) {
+                Crashlytics.logException(Exception(message))
+            } else {
+                Crashlytics.logException(t)
+            }
+        }
+    }
 }
+
+
+
+
