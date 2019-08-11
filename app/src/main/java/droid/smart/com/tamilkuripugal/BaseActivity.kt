956f671ecd119d7d82d2698444d0b18bcd94912a
SmartDroidies <smartdroidies@gmail.com>
2019-08-11 04:07:39 -0400

    Confirm on Exit

---
+++ app/src/main/java/droid/smart/com/tamilkuripugal/BaseActivity.kt
@@ -0,0 +1,46 @@
+package droid.smart.com.tamilkuripugal
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import droid.smart.com.tamilkuripugal.ui.AppExitDialogFragment
+import timber.log.Timber
+
+
+open class BaseActivity : AppCompatActivity(), AppExitDialogFragment.AppExitDialogListener {
+
+
+    override fun onExitConfirm(dialog: DialogFragment) {
+        super.onBackPressed()
+    }
+
+    override fun onExitCancel(dialog: DialogFragment) {
+        Timber.i("User cancelled window")
+    }
+
+    override fun onExitRateme(dialog: DialogFragment) {
+        rateApp()
+        super.onBackPressed()
+    }
+
+    private fun rateApp() {
+        try {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + this.packageName)
+                )
+            )
+        } catch (e: ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + this.packageName)
+                )
+            )
+        }
+    }
+
+}
